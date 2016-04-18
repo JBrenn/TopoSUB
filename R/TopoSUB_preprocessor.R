15336efd4d0395a196a0fcca677085cb7ebd6159
z7H4AabqdYVj <jobrenne@outlook.com>
2016-04-18 15:40:28 +0200

    uniform_clas NULL -> NA

--- R/TopoSUB_preprocessor.R
+++ R/TopoSUB_preprocessor.R
@@ -18,7 +18,7 @@
 TopoSUB_preprocessor <- function(location.file="locations.txt", setup.file="setup.txt", 
                                   PredNamesList=list(topo=c("dem", "slp", "svf"),
                                                       clas=c("landcover", "soil")),
-                                  mode_ls="ols", uniform_class = c(landcover=NULL, soil=NULL))
+                                  mode_ls="ols", uniform_class = c(landcover=NA, soil=NA))
 {
   #load libs raster and geotopbricks (shouldn't be loaded in function but installed/sourced before!)
   # no more necessary when package toposub is used
@@ -289,10 +289,10 @@ TopoSUB_preprocessor <- function(location.file="locations.txt", setup.file="setu
   
   names(lsp) <- c("members", "id", names(samp_mean)[-1], "asp")
   
-  if (!is.null(uniform_class["landcover"]))
+  if (!is.na(uniform_class["landcover"]))
     lsp$landcover <- uniform_class["landcover"]
   
-  if (!is.null(uniform_class["soil"]))
+  if (!is.na(uniform_class["soil"]))
     lsp$soil <- uniform_class["soil"]
     
   write.table(lsp,paste(esPath, '/listpoints.txt' ,sep=''), sep=',', row.names=FALSE, quote=FALSE)
